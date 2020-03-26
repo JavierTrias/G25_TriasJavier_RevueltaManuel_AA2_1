6275468024229726188a611da468e7e209d57bbe
JavierTrias <60731362+JavierTrias@users.noreply.github.com>
2020-03-26 21:09:14 +0100

    Add files via upload

---
+++ Prog2AA2/BombermanAA2/InputManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <Windows.h>
+
+void GetKeys(bool Keys[9]) // Order: W, A, S, D, Esc, Up, Down, Right, Left
+{
+	Keys[0] = GetAsyncKeyState(0x57); // W
+	Keys[1] = GetAsyncKeyState(0x41); // A
+	Keys[2] = GetAsyncKeyState(0x53); // S
+	Keys[3] = GetAsyncKeyState(0x44); // D
+	Keys[4] = GetAsyncKeyState(VK_ESCAPE);
+	Keys[5] = GetAsyncKeyState(VK_UP);
+	Keys[6] = GetAsyncKeyState(VK_DOWN);
+	Keys[7] = GetAsyncKeyState(VK_RIGHT);
+	Keys[8] = GetAsyncKeyState(VK_LEFT);
+}
---
+++ Prog2AA2/BombermanAA2/Main.cpp
@@ -0,0 +1,12 @@
+#include "Constants.h"
+#include "InputManager.h"
+#include "Map.h"
+#include "Player.h"
+#include "Types.h"
+#include <Windows.h>
+
+int main()
+{
+	bool Keys[9] = { false, false, false, false, false, false, false, false, false };
+	GetKeys(Keys[9]);
+}
